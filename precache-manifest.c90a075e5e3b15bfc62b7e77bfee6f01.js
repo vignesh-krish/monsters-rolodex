0930bb27edda4cea7ce6bfd4b6f7188bba8de5a8
vignesh-krish <vigneshsk.jmc@gmail.com>
2020-05-04 13:12:17 +0530

    Updates

---
+++ precache-manifest.c90a075e5e3b15bfc62b7e77bfee6f01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1f7856ce66afbd1a759051c96d4cde4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "816961553649dcea170d",
+    "url": "/monsters-rolodex/static/css/main.7e238807.chunk.css"
+  },
+  {
+    "revision": "d5b7dc0e95c86de42d59",
+    "url": "/monsters-rolodex/static/js/2.0b9572cb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.0b9572cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "816961553649dcea170d",
+    "url": "/monsters-rolodex/static/js/main.67c8c6df.chunk.js"
+  },
+  {
+    "revision": "779c1edafb9d6ee57b27",
+    "url": "/monsters-rolodex/static/js/runtime-main.094e9a2b.js"
+  }
+]);
